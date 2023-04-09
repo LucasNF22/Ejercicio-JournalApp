39f763ba4d7b0c514ffde1e33ea51d37740914af
LucasNF22 <lucasnfiorentino@gmail.com>
2023-04-09 18:31:31 -0300

    Configuracion de themes en MUI

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,6 +1,11 @@
+import { Typography } from "@mui/material"
+import { MailOutline } from "@mui/icons-material"
 
 export const JournalPage = () => {
   return (
-    <div>JournalPage</div>
+    <>
+      <Typography variant='h3' >JournalPage</Typography>
+      <MailOutline />
+    </>
   )
 }
