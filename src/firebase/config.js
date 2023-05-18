d1a9ed0343a40af69173f6942147f9bfc3687c56
LucasNF22 <lucasnfiorentino@gmail.com>
2023-05-17 22:18:26 -0300

    Inicio de configuracion para realizar pruebas en journal/thunks, y configuracion de nueva base de datos en firebase para realizar el testing

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -7,13 +7,25 @@ import { getFirestore } from 'firebase/firestore/lite'
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
+//Dev-Prod
+// const firebaseConfig = {
+//   apiKey: "AIzaSyB6W6N4jh7z0AKYWcHGIw15jFDowjpWmbc",
+//   authDomain: "curso-react-10394.firebaseapp.com",
+//   projectId: "curso-react-10394",
+//   storageBucket: "curso-react-10394.appspot.com",
+//   messagingSenderId: "731733521031",
+//   appId: "1:731733521031:web:26e74a3ebaa3e601b67e22"
+// };
+
+//Testing
 const firebaseConfig = {
-  apiKey: "AIzaSyB6W6N4jh7z0AKYWcHGIw15jFDowjpWmbc",
-  authDomain: "curso-react-10394.firebaseapp.com",
-  projectId: "curso-react-10394",
-  storageBucket: "curso-react-10394.appspot.com",
-  messagingSenderId: "731733521031",
-  appId: "1:731733521031:web:26e74a3ebaa3e601b67e22"
+  apiKey: "AIzaSyDyGWniGTnJrSFFPKvQfLgZjK8M_8hrFpQ",
+  authDomain: "testing-firebase-5b58d.firebaseapp.com",
+  projectId: "testing-firebase-5b58d",
+  storageBucket: "testing-firebase-5b58d.appspot.com",
+  messagingSenderId: "756133884827",
+  appId: "1:756133884827:web:d5878cfa88edbcbb3aa73e"
 };
 
 // Initialize Firebase
--- testing/store/journal/thunks.test.js
+++ testing/store/journal/thunks.test.js
@@ -1,9 +1,18 @@
+import { startNewNote } from "../../../src/store/journal/thunks";
 
-describe('Pruebas en el journal Thukns', () => {
+describe('Pruebas en el journal Thunks', () => {
 
-    test('debe de ', () => {
+    const dispatch = jest.fn();
+    const getState = jest.fn();
 
+    beforeEach( () => jest.clearAllMocks() )
 
+    test('startNewNote debe crear una nueva nota en blanco', async() => {
+
+        const uid = 'TEST-UID'
+        getState.mockReturnValue({auth: { uid: uid }})
+
+        await startNewNote()( dispatch, getState );
         
     })
 
