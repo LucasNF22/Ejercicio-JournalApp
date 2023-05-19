3825faabab935ccaf5a07a164ea936945e9dc129
LucasNF22 <lucasnfiorentino@gmail.com>
2023-05-18 23:34:21 -0300

    Finalizacion de pruebas en startNewNote, con funcion recursiva para eliminar notas basuras creadsa en firebase.

--- testing/store/journal/thunks.test.js
+++ testing/store/journal/thunks.test.js
@@ -1,4 +1,8 @@
+import { collection, deleteDoc, getDocs } from "firebase/firestore/lite";
+import { setActiveNote } from "../../../src/store/journal/journalSlice";
+import { addEmptyNote, savingNewNote } from "../../../src/store/journal/journalSlice";
 import { startNewNote } from "../../../src/store/journal/thunks";
+import { FirebaseDB } from "../../../src/firebase/config";
 
 describe('Pruebas en el journal Thunks', () => {
 
@@ -13,7 +17,31 @@ describe('Pruebas en el journal Thunks', () => {
         getState.mockReturnValue({auth: { uid: uid }})
 
         await startNewNote()( dispatch, getState );
-        
+
+        expect( dispatch ).toHaveBeenCalledWith( savingNewNote() );
+        expect( dispatch ).toHaveBeenCalledWith( addEmptyNote({
+            body: '',
+            title: '',
+            imageUrls: [],
+            date: expect.any( Number ) ,
+            id: expect.any( String ) 
+        }) );
+        expect( dispatch ).toHaveBeenCalledWith( setActiveNote({
+            body: '',
+            title: '',
+            imageUrls: [],
+            date: expect.any( Number ) ,
+            id: expect.any( String ) 
+        }) );
+
+        // Borrar de firebase
+        const collectionRef = collection( FirebaseDB, `${uid}/journal/notes` );
+        const docs = await getDocs( collectionRef );
+
+        const deletePromises = [];
+        docs.forEach( doc => deletePromises.push( deleteDoc( doc.ref ) ) ) // funcion recursiva para eliminar datos de firebase
+        await Promise.all( deletePromises );
+
     })
 
 })
