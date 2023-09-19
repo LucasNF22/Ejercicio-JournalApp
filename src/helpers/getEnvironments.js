54bca08c6e70f2243ab745bf2d8864fc02f0ea64
LucasNF22 <lucasnfiorentino@gmail.com>
2023-09-19 10:09:03 -0300

    limpieza getenviorments

--- src/helpers/getEnvironments.js
+++ src/helpers/getEnvironments.js
@@ -1,17 +1,10 @@
 
 export const getEnvironments = () => {
 
-    // import.meta.env;
 
     return{
         ...import.meta.env
     }
 
-    // return {
-    //   define: {   
-    //     ...import.meta.env,
-    //       __APP_ENV__: process.env.VITE_VERCEL_ENV,
-    //     },
-    //   };
-
+  
 }
